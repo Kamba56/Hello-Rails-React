812e6ff2a21db1197af9d1165ed8b0e28dc0cc7d
Kamba56 <tomajohn316@yahoo.com>
2023-01-28 20:31:00 +0100

    Add contoleers

---
+++ app/javascript/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import Greeting from './Greeting';
+
+const App = () => {
+    return (
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/" element={<Greeting  />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    );
+}
+
+export default App;
